b21fd0282958e20d5a7702ea91e6cf88f78574d2
HayatGnd <hayatguendoul98@gmail.com>
2025-06-04 21:13:59 +0200

    Mise à jour dossier MigrateSql3

--- MigrateSql3/worldtour/asiatouragency/models.py
+++ MigrateSql3/worldtour/asiatouragency/models.py
@@ -7,3 +7,7 @@ class Tour(models.Model):
     destination = models.CharField(max_length=100)
     number_of_nights = models.IntegerField()
     price = models.IntegerField()
+    
+    #String representation of the model
+    def __str__ (self):
+        return (f"ID:{self.id}: From {self.origin_country} to {self.destination} for {self.number_of_nights} nights at {self.price} $")
